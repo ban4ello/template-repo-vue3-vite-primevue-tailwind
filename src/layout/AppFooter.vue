58072cc325d07943ee58d1f10fbca7e37519d199
ban4ello <ban4ellog@gmail.com>
2025-01-19 17:14:24 +0300

    init commit

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -2,9 +2,6 @@
 
 <template>
   <div class="layout-footer">
-    "Calculations and Analysis" by
-    <a href="https://ban4ello.github.io/resume/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">
-      MIRI
-    </a>
+    Vue3 App. Footer-component
   </div>
 </template>
--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/report-documentation' : '/',
   base: '/',
   optimizeDeps: {
     noDiscovery: true
